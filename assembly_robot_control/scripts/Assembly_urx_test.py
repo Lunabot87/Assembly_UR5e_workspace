a542a708dd520fbd4b0202affb4b5a0ece54e630
Lunabot87 <rkwoskek@naver.com>
2020-07-15 15:25:35 +0900

    param update

--- assembly_robot_control/scripts/Assembly_urx_test.py
+++ assembly_robot_control/scripts/Assembly_urx_test.py
@@ -5,12 +5,12 @@ import sys
 import math as m
 import numpy as np
 import copy
+import random
 
 sys.path.append('../../assembly_robot_gripper_control/scripts')
 from Robotiq_2f_Gripper import Robotiq_2f_Gripper
 
-start_pose = [0.15697152980980725, -0.4958977011271538, 0.24435886093454873, 
-            -2.221022989086017, -2.2214727795467892, 0.0004896852898812067]
+start_pose = [0.01934323142607855, -0.753240435450635, 0.24928563206499854, 3.1388263581464297, 0.00010558903307717413, 0.00015309316489978483]
 
 def _format_move(command, tpose, acc, vel, radius=0, prefix=""):
     tpose = [round(i, 6) for i in tpose]
@@ -20,23 +20,25 @@ def _format_move(command, tpose, acc, vel, radius=0, prefix=""):
     return "{}({}[{},{},{},{},{},{}], a={}, v={}, r={})".format(command, prefix, *tpose)  
 
 def get_spiral_cmd(initial):
-    vel = 0.3
-    acc = 0.1
+    vel = 0.5
+    acc = 0.25
     radius = 0.0005
 
     force_mod = [0,0,1,0,0,0]
-    force_toq = [0,0,1,0,0,0] 
+    force_toq = [0,0,4,0,0,0] 
 
     # spiral motion
-    R = 0.01
-    dth = m.pi/30
-    dr = R*0.005
-    th_array = np.arange(0, 6*m.pi+dth, step=dth)
-    r_array = np.arange(0, R, step=dr)
-    # print th_array
-    spiral_list = []
+    R = 0.006
+    revolution = 8
+    dth = m.pi/10
+    th_len_1 = int(revolution*2*m.pi/dth) # len(th_array)-1
+    dr = R/th_len_1 # R*0.005
 
-    # get initial pose
+    th_array = np.arange(0, revolution*2*m.pi+dth, step=dth)    
+    r_array = np.arange(0, R+dr, step=dr)    
+    # print th_array
+
+    spiral_list = []
     spiral_list.append(initial)
     i = 0
     for th in th_array:
@@ -50,8 +52,8 @@ def get_spiral_cmd(initial):
 
     # cmd
     cmd_str  = "def sprial():\n"
+    cmd_str += "\tforce_mode_set_damping(0.0)\n"
     cmd_str += "\tthread Thread_1():\n"
-    cmd_str += "\t\tforce_mode_set_damping(0)\n"
     cmd_str += "\t\twhile (True):\n"
     cmd_str += "\t\t\tforce_mode(tool_pose(), "+str(force_mod) +"," + str(force_toq) +", 2, [0.1, 0.1, 0.15, 0.17, 0.17, 0.17])\n"
     cmd_str += "\t\t\tsync()\n"
@@ -71,27 +73,47 @@ def get_spiral_cmd(initial):
 
     return cmd_str
 
+# def wait_for_move()
+
 def main():
     global start_pose
 
     #rob1grip = Robotiq_2f_Gripper("192.168.13.101") #action_gripper(action_pos, wait_time = 2) open = 0, close = 255
     rob1 = urx.Robot("192.168.13.101", use_rt=True)
-    
+
     # move to initial pose
-    # rob1.movel(start_pose, wait=False)
+    error = 0.006
+    random_error_x = error*random.uniform(-1, 1)
+    random_error_y = error*random.uniform(-1, 1)
+    # random_error_x = 0
+    # random_error_y = -error
+    start_pose[0] += random_error_x
+    start_pose[1] += random_error_y
+    print "error_x={}, error_y={}".format(random_error_x, random_error_y)
+    rob1.movel(start_pose, wait=True)
 
-
+    while(True):
+        current = rob1.getl()
+        dist = np.linalg.norm(np.array(current)-np.array(start_pose))
+        if dist < 0.001:
+            break
+    
+    rob1.send_program("zero_ftsensor()")
+    time.sleep(0.2)
+    
     # go down
+    print "="*20 +"go_down"+"="*20
     force_mod = [0,0,1,0,0,0]
     force_toq = [0,0,1,0,0,0] 
 
     cmd_str  = "def go_down():"
+    cmd_str += "\tforce_mode_set_damping(0.005)\n"
     cmd_str += "\twhile (True):\n"
-    cmd_str += "\t\tforce_mode_set_damping(0.005)\n"
     cmd_str += "\t\tforce_mode(tool_pose(), "+str(force_mod) +"," + str(force_toq) +", 2, [0.1, 0.1, 0.15, 0.17, 0.17, 0.17])\n"
     cmd_str += "\t\tsync()\n"
     cmd_str += "\tend\n"
     cmd_str += "end\n"
+    time.sleep(0.1)
     rob1.send_program(cmd_str)
 
     while(True):
@@ -111,13 +133,15 @@ def main():
     initial = rob1.getl()
     spiral_cmd = get_spiral_cmd(initial)
     # print spiral_cmd
+    # rob1.send_program("force_mode_set_damping(0)")
+    # time.sleep(0.1)
     rob1.send_program(spiral_cmd)
 
     while(True):
         try:
             force = rob1.get_tcp_force()
             print force[0], force[1]
-            if abs(force[0]) > 3 or abs(force[1]) > 3:
+            if abs(force[0]) > 7 or abs(force[1]) > 7:
                 rob1.send_program("end_force_mode()")
                 break
         except KeyboardInterrupt:
@@ -125,13 +149,16 @@ def main():
             break
     time.sleep(0.1)
 
+
+    # spiral check
+
     # real insert
     print "="*20 +"real_insert"+"="*20
     force_mod = [1,1,1,0,0,0]
     force_toq = [0,0,50,0,0,0]
     cmd_str  = "def real_insert():"
+    cmd_str += "\tforce_mode_set_damping(0.005)\n"
     cmd_str += "\twhile (True):\n"
-    cmd_str += "\t\tforce_mode_set_damping(0.005)\n"
     cmd_str += "\t\tforce_mode(tool_pose(), "+str(force_mod) +"," + str(force_toq) +", 2, [0.1, 0.1, 0.15, 0.17, 0.17, 0.17])\n"
     cmd_str += "\t\tsync()\n"
     cmd_str += "\tend\n"
@@ -142,7 +169,7 @@ def main():
         try:
             force = rob1.get_tcp_force()
             print force[2]
-            if force[2] > 30:
+            if force[2] > 50:
                 rob1.send_program("end_force_mode()")
                 break
         except KeyboardInterrupt:
