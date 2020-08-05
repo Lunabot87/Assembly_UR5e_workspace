804b1f1f3ebfe8041cce102b4216c164e6641d13
U-lo-l <jeffk0408@naver.com>
2020-08-05 21:34:31 +0900

    ulol : rotate_ee_by_base & move_ee_by_base added

--- assembly_robot_control/ulol_tf/ulol_UR5.py
+++ assembly_robot_control/ulol_tf/ulol_UR5.py
@@ -1,4 +1,4 @@
-import tf
+
 import sys
 import copy
 import random
@@ -8,22 +8,41 @@ import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
 
-from math import pi, sqrt
+from math import pi, sqrt, acos, sin, cos
+from numpy.linalg import inv as Inv
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
 from nav_msgs.msg import Odometry
 from tf.transformations import*
+from tf import*
 
 from part_info import*          # part_file_address, part_name
 
 def make_orientation_list(orientation):
-  o_list = [orientation.x, orientation.y, orientation.z, orientation.w]
-  return o_list
- 
+    o_list = [orientation.x, orientation.y, orientation.z, orientation.w]
+    return o_list
+def make_position_list(position):
+    p_list = [position.x,position.y,position.z]
+    return p_list
+def pose_from_transrot(trans = [0,0,0],rot = [0,0,0]):
+    pose = geometry_msgs.msg.PoseStamped().pose
+    pose.position.x = trans[0]
+    pose.position.y = trans[1]
+    pose.position.z = trans[2]
+
+    o_list = quaternion_from_euler(rot[0],rot[1],rot[2])
+    pose.orientation.x = o_list[0]
+    pose.orientation.y = o_list[1]
+    pose.orientation.z = o_list[2]
+    pose.orientation.w = o_list[3]
+
+    return pose
+
 class UR5(object):
     def __init__(self,G_name = 'rob1_arm'):
         super(UR5, self).__init__()
         moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node('ulol_UR5', anonymous=True)
 
         self.robot = moveit_commander.RobotCommander()
         self.scene = moveit_commander.PlanningSceneInterface()
@@ -31,12 +50,20 @@ class UR5(object):
         group_names = self.robot.get_group_names()
 
         self.group_name = G_name
+        
+        self.real_base_name = G_name.split('_')[0]+'_real_base_link'
+
         self.move_group = moveit_commander.MoveGroupCommander(self.group_name)
         self.planning_frame = self.move_group.get_planning_frame()
         self.eef_link = self.move_group.get_end_effector_link()
      
+    def send_TF(self,pose,tf_name):
+        position_list = make_position_list(pose.position)
+        orientation_list = make_orientation_list(pose.orientation)
+        self.br.sendTransform(position_list, orientation_list, rospy.Time.now(), tf_name, 'world')
+
     def go_to_initial_pose(self):
-        joint_goal = [-pi/2,-pi/2,-pi/2,-pi/2,pi/2,pi]
+        joint_goal = [-pi/2,-pi/2,-pi/2,-pi/4,pi/2,pi]
         self.move_group.go(joint_goal, wait=True)
         self.move_group.stop()
 
@@ -50,25 +77,11 @@ class UR5(object):
         self.move_group.stop()
         self.move_group.clear_pose_targets()
 
-    def execute_plan(self, plan):
-        self.move_group.execute(plan, wait=True)
-
     def move_cartesian_path(self, pose):
         waypoint = []
         waypoint.append(copy.deepcopy(pose))
         (plan, fraction) = self.move_group.compute_cartesian_path(waypoint,0.01,0.0)
         self.move_group.execute(plan,wait = True)
-
-    def add_cartesian_pose(self, trans):
-        pose = self.move_group.get_current_pose().pose
-        pose.position.x += trans[0]
-        pose.position.y += trans[1]
-        pose.position.z += trans[2]
-        waypoint = []
-        waypoint.append(copy.deepcopy(pose))
-        (plan, fraction) = self.move_group.compute_cartesian_path(waypoint,0.01,0.0)
-        self.move_group.execute(plan,wait = True)
-
 
     def get_target_orientation(self,orientation): # have to be modified in rotation
         
@@ -91,37 +104,7 @@ class UR5(object):
 
         return new_orientation
 
-    def get_random_pose(self, x = random.uniform(-0.35,0.35), y = random.uniform(0,0.4) - 0.745
-                                , z = random.uniform(0.2,0.3), roll = random.uniform(-3.14,3.14)
-                                , pitch = random.uniform(-3.14,3.14), yaw = random.uniform(-3.14,3.14)):
-        
-        orientation_list = quaternion_from_euler(roll,pitch,yaw)
-
-        pose = self.move_group.get_current_pose().pose
-        pose = geometry_msgs.msg.PoseStamped().pose
-        pose.position.x = x
-        pose.position.y = y
-        pose.position.z = z
-        pose.orientation.x = orientation_list[0]
-        pose.orientation.y = orientation_list[1]
-        pose.orientation.z = orientation_list[2]
-        pose.orientation.w = orientation_list[3]
-
-        return pose
-
-
-        move_group = self.move_group
-        joint_goal = move_group.get_current_joint_values()
-        joint_goal[4] -= pi
-        joint_goal[3] -= pi/2
-        if xyz_length[0] >= xyz_length[1]:
-          joint_goal[5] = pi
-        else:
-          joint_goal[5] = pi/2
-        move_group.go(joint_goal, wait=True)
-        move_group.stop()
-
-    def grab_part(self, pose, z_offset):
+    def go_to_grab_ready_pose(self, pose, z_offset):
         target_pose = copy.deepcopy(pose)
         target_pose.orientation = self.get_target_orientation(target_pose.orientation)
 
@@ -141,31 +124,7 @@ class UR5(object):
         target_pose.position.y += z_offset * z_axis[1]
         target_pose.position.z += z_offset * z_axis[2]
         self.move_cartesian_path(target_pose)
-        # print "[INFO] Ready to attach"    
 
-        # print "PRESS ENTER TO ATTACH"
-        # raw_input()        
-        # self.attach_part(attach_list)
-        # print "[INFO] Object attached"
-
-        # print "PRESS ENTER TO MOVE-UP"
-        # raw_input()
-        # target_pose.position.z += 0.3
-        # target_pose.position.x += 0.1
-        # self.move_cartesian_path(target_pose)
-
-        # print "PRESS ENTER TO MOVE_DOWN"
-        # raw_input()
-        # target_pose.position.z -= z_offset
-        # self.move_cartesian_path(target_pose)
-        # print "[INFO] Ready to detach"
-
-
-        # print "PRESS ENTER TO DETACH"
-        # raw_input()
-        # self.detach_part(attach_list)
-
-        
     def attach_part(self, attach_list):
         if self.group_name == 'rob1_arm':
             grasping_group = 'rob1_hand'
@@ -191,6 +150,94 @@ class UR5(object):
         for attaced_pin_name in attach_list['pin']:
             self.scene.remove_attached_object(self.eef_link, attaced_pin_name)
 
-    # def attached_part(self,name=[]):
-    #     A = self.scene.get_attached_objects(name)
-    #     print A
+    def rotate_ee_by_base(self,rot):
+        rotating_matrix = euler_matrix(rot[0],rot[1],rot[2])[:3,:3]
+
+        base_rot = self.move_group.get_current_rpy(self.real_base_name)
+        base_rot_matrix = euler_matrix(base_rot[0],base_rot[1],base_rot[2])[:3,:3]
+
+        ee_rot = self.move_group.get_current_rpy(self.eef_link)
+        ee_rot_matrix = euler_matrix(ee_rot[0],ee_rot[1],ee_rot[2])[:3,:3]
+
+        Rot_ee_from_base = ee_rot_matrix.dot(Inv(base_rot_matrix))
+
+        new_rot_matrix = rotating_matrix.dot(ee_rot_matrix)
+        new_rot = euler_from_matrix(new_rot_matrix)
+
+        new_pose = pose_from_transrot(rot = new_rot)
+        return new_pose
+
+    def move_ee_by_base(self,trans,rot = [0,0,0]):#trans = [x,y,z], rot = [r,p,y]
+
+        base_rot = self.move_group.get_current_rpy(self.real_base_name)
+        base_rot_matrix = euler_matrix(base_rot[0],base_rot[1],base_rot[2])[:3,:3]
+        new_trans = base_rot_matrix.dot(trans)
+
+        pose = self.rotate_ee_by_base(rot)
+        pose.position = self.move_group.get_current_pose().pose.position
+        pose.position.x += trans[0]
+        pose.position.y += trans[1]
+        pose.position.z += trans[2]
+
+        return pose
+
+
+def example_for_test():
+    try:
+        R1 = UR5()
+        R2 = UR5('rob2_arm')
+        br = TransformBroadcaster()
+        R1.go_to_initial_pose()
+        R2.go_to_initial_pose()
+
+      
+        cur_pose = R1.move_group.get_current_pose().pose
+
+        #========================VISUALIZ_TF========================
+        position_list = make_position_list(cur_pose.position)
+        orientation_list = make_orientation_list(cur_pose.orientation)
+        br.sendTransform(position_list, orientation_list, rospy.Time.now(), 'PREVIOUS', 'world')
+        #===========================================================
+
+        r = [0,0.785,0]
+        pose1 = R1.move_ee_by_base([0,0,0],r)
+
+        #========================VISUALIZ_TF========================
+        position_list = make_position_list(pose1.position)
+        orientation_list = make_orientation_list(pose1.orientation)
+        br.sendTransform(position_list, orientation_list, rospy.Time.now(), 'NEW', 'world')
+        #===========================================================
+
+        print"PRESS ENTER TO MOVE",
+        raw_input()
+        R1.move_cartesian_path(pose1)
+
+        cur_pose = R2.move_group.get_current_pose().pose
+
+        #========================VISUALIZ_TF========================
+        position_list = make_position_list(cur_pose.position)
+        orientation_list = make_orientation_list(cur_pose.orientation)
+        br.sendTransform(position_list, orientation_list, rospy.Time.now(), 'PREVIOUS', 'world')
+        #===========================================================
+
+        r = [0,0,0.785]
+        pose2 = R2.move_ee_by_base([0,0,0],r)
+
+        #========================VISUALIZ_TF========================
+        position_list = make_position_list(pose2.position)
+        orientation_list = make_orientation_list(pose2.orientation)
+        br.sendTransform(position_list, orientation_list, rospy.Time.now(), 'NEW', 'world')
+        #===========================================================
+
+        print"PRESS ENTER TO MOVE",
+        raw_input()
+        R2.move_cartesian_path(pose2)
+
+    except rospy.ROSInterruptException:
+        return
+    except KeyboardInterrupt:
+        return
+
+
+if __name__ == '__main__':
+    example_for_test()
