cd99dc06e55eefc96414dec1ae6bf1b56831c205
Lunabot87 <rkwoskek@naver.com>
2021-03-11 05:27:35 +0900

    table_hole_insert update

--- assembly_robot_control/test/table_hole_urx_control.py
+++ assembly_robot_control/test/table_hole_urx_control.py
@@ -5,34 +5,46 @@ import math as m
 import numpy as np
 import copy
 import random
+import time
 
 class UrxMotion():
 
     def __init__(self, robot_ip):
-        self.robot_ip = robot_ip
         self.reset()
-        self.robot.send_program(self._set_gripper())
+        self.rob1.send_program(self._set_gripper())
+        self.rob2.send_program(self._set_gripper())
         time.sleep(3)
 
+        self.pin_pose = [[-0.03045, -1.96946, -1.80458 -0.93844 1.5692 -0.02784],[-0.08934, -1.99052, -1.77228 -0.94964 1.5691 -0.08679],[-0.03276, -1.89556, -1.91412 -0.90275 1.5692 -0.02986],[-0.09810, -1.91374, -1.88767 -0.91104 1.5692 -0.09525],[-0.03553, -1.81349, -2.02856 -0.87075 1.5694 -0.03246],[-0.10267, -1.83680, -1.99677 -0.87891 1.5693 -0.09958]]
+        self.pin_pre_pose = [[-0.02975, -1.784503, -1.282465, -1.6455, 1.5691, -0.027005],[-0.08853, -1.809195, -1.282967, -1.6204, 1.5691, -0.086079],[-0.03197, -1.695997, -1.348806, -1.6677, 1.5692, -0.029000],[-0.09740, -1.714498, -1.384382, -1.6136, 1.5692, -0.094405],[-0.03477, -1.602397, -1.402856, -1.7075, 1.5694, -0.031513],[-0.10202, -1.621245, -1.527932, -1.5632, 1.5694, -0.098787]]
+
+        self.hole_pose = [[-1.298815552, -1.901123662, -1.85568714, -0.955488995, 1.5698800087, 0.2749400138],[-1.417055908, -1.899667402, -1.92237186, -0.890888051, 1.5710926055, 0.1558179855],[-0.642283741, -2.048429628, -1.69451618, -0.970111684, 1.5706362724, 0.9299578666]]
+
+        self.pre_hand_over = [[-1.2928, -1.4172, -1.4987, -0.2354, 1.2902, 1.5710] ,[-0.7087, -1.0365, -1.7401, -0.3599, 0.7054, 1.5706]]
+        self.hand_over = [[-1.3860, -1.8260, -1.0690, -0.2458, 1.3844, 1.5692],[-1.1955, -1.6401, -1.2864, -0.2102, 1.1927, 1.5721]]
+
+
+        
 
     def reset(self):
         connected = False 
         while not connected:
             try:         
-                self.robot = urx.URRobot(self.robot_ip, use_rt=True)        
+                self.rob1 = urx.URRobot("192.168.13.101", use_rt=True) 
+                self.rob2 = urx.URRobot("192.168.13.100", use_rt=True)        
                 connected = True    
             except:     
-                time.sleep(1)  
+                time.sleep(1)
 
 
-    def force_control(self):
-        self.robot.send_program("zero_ftsensor()")
+    def force_control(self, robot):
+        robot.send_program("zero_ftsensor()")
         time.sleep(0.2)
 
         print "="*20 +"go_down"+"="*20
       
-        force_mod = [0,0,1,0,pitch,0]
-        force_toq = [0,0,-5,0,0,0] 
+        force_mod = [1,1,1,0,0,0]
+        force_toq = [0,0,-1,0,0,0] 
 
         cmd_str  = "def go_down():"
         cmd_str += "\tforce_mode_set_damping(0.005)\n"
@@ -42,19 +54,19 @@ class UrxMotion():
         cmd_str += "\tend\n"
         cmd_str += "end\n"
         time.sleep(0.1)
-        self.robot.send_program(cmd_str)
+        robot.send_program(cmd_str)
         time.sleep(0.2)
 
-
+        start = robot.getl()
         while(True):
             try:
-                force = self.robot.get_tcp_force()
+                force = robot.get_tcp_force()
 
-                if abs(force[2]) > 3:
-                    self.robot.send_program("end_force_mode()")
+                if start[2] - robot.getl()[2] > 0.04:
+                    robot.send_program("end_force_mode()")
                     break
             except KeyboardInterrupt:
-                self.robot.send_program("end_force_mode()")
+                robot.send_program("end_force_mode()")
                 break
 
 
@@ -105,7 +117,7 @@ class UrxMotion():
         msg += "\t\trq_pos = socket_get_var(\"POS\",\"gripper_socket\")\n"
         #msg += "\t\ttextmsg(\"rq_pos:\", rq_pos)\n"
         msg += "\t\tif norm(rq_pos_1 - rq_pos) < 0.5:\n"
-        msg += "\t\t\tset_digital_out(7, True)\n"
+        msg += "\t\t\tset_digital_out(10, True)\n"
         msg += "\t\t\tbreak\n"
         msg += "\t\telse:\n"
         msg += "\t\t\trq_pos_1 = rq_pos\n"
@@ -115,116 +127,148 @@ class UrxMotion():
 
         return msg
 
+    def gripper_move_and_wait(self, robot ,pos):
+        robot = robot
 
-    def init_pose(self):
-        self.robot.movej([-1.5708006064044397, -1.04721291482959, -1.5707988739013672, -1.5739270649352015, 1.5707921981811523, 5.3882598876953125e-05])
+        robot.send_program(self._gripper_move(pos))
+
+        while True:
+            if robot.get_digital_out(10) != 0:
+                robot.send_program("set_digital_out(7, False)")
+                print "gripper_move_and_wait complete"
+                break
+            else:
+                continue
 
 
-    def pose_z_axis_up(self, z_offset):
-        goal_pose = self.robot.getl()
+    def init_pose(self, robot):
+        robot.movej([-1.5708006064044397, -1.04721291482959, -1.5707988739013672, -1.5739270649352015, 1.5707921981811523, 5.3882598876953125e-05])
+
+
+    def pose_z_axis_up(self, robot, z_offset):
+        goal_pose = robot.getl()
         goal_pose[2] += z_offset
 
-        self.robot.movel(goal_pose)
+        robot.movel(goal_pose)
 
-    def pose_y_axis_up(self, z_offset):
-        goal_pose = self.robot.getl()
+    def pose_y_axis_up(self, robot, z_offset):
+        goal_pose = robot.getl()
         goal_pose[1] += z_offset
 
-        self.robot.movel(goal_pose)
+        robot.movel(goal_pose)
+
+
+    def handover(self, hole_num):
+
+        self.rob1.movej(self.hand_over_pre_pose[0])
+
+        self.rob2.movej(self.hand_over_pre_pose[1])
+
+        self.rob1.movej(self.hand_over_pose[0])
+
+        self.rob2.movej(self.hand_over_pose[1])
+
+        if hole_num == 1:
+            self.gripper_move_and_wait(self.rob1, 255)
+
+            self.gripper_move_and_wait(self.rob2, 0)
+
+            self.rob1.movej(self.hand_over_pre_pose[0])
+
+            self.rob2.movej(self.hand_over_pre_pose[1])
+
+            self.init_pose(self.rob2)
+
+        else:
+            self.gripper_move_and_wait(self.rob2, 255)
+
+            self.gripper_move_and_wait(self.rob1, 0)
+
+            self.rob1.movej(self.hand_over_pre_pose[0])
+
+            self.rob2.movej(self.hand_over_pre_pose[1])
+
+            self.init_pose(self.rob1)
 
 
 
-def main(): 
-    rob1 = UrxMotion("192.168.13.101")
-    rob2 = UrxMotion("192.168.13.100")
+    def insert_hole(self, hole_num, pin_num):
+        if hole_num == 1:
+            #tool table movement
+            time.sleep(10)
 
-    rob1.init_pose()
-    rob2.init_pose()
+            self.gripper_move_and_wait(self.rob1, 0) #gripper open
+            self.gripper_move_and_wait(self.rob2, 0) #gripper open
 
-    rob1._gripper_move(0) #gripper open
-    rob2._gripper_move(0) #gripper open
+            self.rob2.movej(self.pin_pre_pose[pin_num - 1])
 
-    rob1.movej(-0.12076789537538701, -1.6519357166686, -1.581700325012207, -1.4943846029094239, 1.5664734840393066, -0.10190803209413701)
+            self.gripper_move_and_wait(self.rob2, 100)
 
-    #pin1 pre pose
-    rob1.movej()
+            self.rob2.movej(self.pin_pose[pin_num - 1])
 
-    rob1._gripper_move(100) #gripper open
+            self.gripper_move_and_wait(self.rob2, 255)
 
-    rob1.pose_z_axis_up(0.3)
+            self.pose_z_axis_up(self.rob2, 0.3)
 
+            self.handover(hole_num)
 
-    #hand_over_pre_pose
-    rob1.movej()
-    rob2.movej()
+            self.rob1.movej(self.hole_pose[hole_num - 1])
 
+            self.force_mod(rob1)
 
-    rob1.pose_y_axis_up(-0.3)
-    rob2.pose_y_axis_up(-0.3)
+            self.gripper_move_and_wait(self.rob1, 0)
 
+            self.pose_z_axis_up(self.rob1, 0.3)
 
-    rob2._gripper_move(255)
-    rob1._gripper_move(0)
-
-    rob1.pose_y_axis_up(0.3)
-    rob2.pose_y_axis_up(0.3)
-
-    rob1.init_pose()
-
-    #hole1 joint
-    rob2.movej()   
-
-    rob2.force_control()
-
-    rob2.pose_z_axis_up(0.05)
-
-    rob2._gripper_move(0) #gripper open
-
-    rob2.init_pose()
+            self.init_pose(self.rob1)
 
 
-    rob1._gripper_move(0) #gripper open
-    rob2._gripper_move(0) #gripper open
+        else:
+            #tool table movement
+            time.sleep(10)
 
-    rob1.movej(-0.12076789537538701, -1.6519357166686, -1.581700325012207, -1.4943846029094239, 1.5664734840393066, -0.10190803209413701)
+            self.gripper_move_and_wait(self.rob1, 0) #gripper open
+            self.gripper_move_and_wait(self.rob2, 0) #gripper open
 
-    #pin1 pre pose
-    rob1.movej()
+            self.rob1.movej(self.pin_pre_pose[pin_num - 1])
 
-    rob1._gripper_move(100) #gripper open
+            self.gripper_move_and_wait(self.rob2, 100)
 
-    rob1.pose_z_axis_up(0.3)
+            self.rob1.movej(self.pin_pose[pin_num - 1])
 
+            self.gripper_move_and_wait(self.rob1, 255)
 
-    #hand_over_pre_pose
-    rob1.movej()
-    rob2.movej()
+            self.pose_z_axis_up(self.rob1, 0.3)
 
+            self.handover(hole_num)
 
-    rob1.pose_y_axis_up(-0.3)
-    rob2.pose_y_axis_up(-0.3)
+            self.rob2.movej(self.hole_pose[hole_num - 1])
 
+            self.force_mod(self.rob2)
 
-    rob2._gripper_move(0)
-    rob1._gripper_move(255)
+            self.gripper_move_and_wait(self.rob2, 0)
 
-    rob1.pose_y_axis_up(0.3)
-    rob2.pose_y_axis_up(0.3)
+            self.pose_z_axis_up(self.rob2, 0.3)
 
-    rob1.init_pose()
+            self.init_pose(self.rob2)
 
-    #hole1 joint
-    rob2.movej()   
+    def main(): 
+        hole_num = []#data input
 
-    rob2.force_control()
+        pin_num = []
 
-    rob2.pose_z_axis_up(0.05)
+        for i in range(3):
+            self.insert_hole(hole_num[i], pin_num[i])
 
-    rob2._gripper_move(255) #gripper open
+        print "insert_end"
+
+    
 
 
     
 
 if __name__ == '__main__':
-    main()
+    robot = UrxMotion()
+
+    robot.main()
 
